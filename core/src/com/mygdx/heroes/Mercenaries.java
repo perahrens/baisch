9b4110eaf77705bb0a705d8d5e4e0c010c28de61
perahrens <per.ahrens@gmx.de>
2018-08-14 22:08:45 +0200

    Started implementation of mercenary functionality

--- core/src/com/mygdx/heroes/Mercenaries.java
+++ core/src/com/mygdx/heroes/Mercenaries.java
@@ -7,15 +7,33 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class Mercenaries extends Hero {
-	public Mercenaries() {
-		heroName = "Mercenaries";
-		heroID = "Merc.";
-		sprite = atlas.createSprite("wp", -1);
 
-		this.isSelected = false;
+  // (0) ready (1) defending (2) dead
+  private int[] mercenaryStates = { 0, 0, 0, 0, 2, 2, 2, 2 };
 
-		setWidth(sprite.getWidth()*scaleFactor);
-		setHeight(sprite.getHeight()*scaleFactor);
-		
-	}
+  public Mercenaries() {
+    heroName = "Mercenaries";
+    heroID = "Merc.";
+    sprite = atlas.createSprite("wp", -1);
+
+    this.isSelected = false;
+
+    setWidth(sprite.getWidth() * scaleFactor);
+    setHeight(sprite.getHeight() * scaleFactor);
+
+  }
+
+  public void recover() {
+    int heals = 4;
+    for (int i = 0; i < mercenaryStates.length; i++) {
+      if (mercenaryStates[i] == 2) {
+        mercenaryStates[i] = 0;
+        heals--;
+      }
+      if (heals == 0) {
+        break;
+      }
+    }
+  }
+
 }

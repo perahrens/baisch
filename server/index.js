f07a4279b5241821af5322598cb4e8132c4803c4
perahrens <per.ahrens@gmx.de>
2018-09-03 18:32:15 +0200

    Enable users to go in ready and waiting state

--- server/index.js
+++ server/index.js
@@ -1,31 +1,51 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var players = [];
+var users = [];
 
 server.listen(8082, function() {
   console.log("Server is now running... ");
 });
 
 io.on('connection', function(socket) {
-  console.log("Player Connected");
+  console.log("User Connected");
   socket.emit('socketID', { id: socket.id });
-  socket.broadcast.emit('newPlayer', { id: socket.id });
+  socket.broadcast.emit('newUser', { id: socket.id });
+
   socket.on('disconnect', function() {
-    console.log("Player Disconnected");
-    socket.broadcast.emit('playerDisconnected', { id:socket.id } );
-    for (var i = 0; i < players.length; i++) {
-      if (players[i].id == socket.id) {
-        players.splice(i);
+    console.log("User Disconnected");
+    socket.broadcast.emit('userDisconnected', { id:socket.id } );
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].id == socket.id) {
+        users.splice(i, 1);
       }
     }
-    socket.broadcast.emit('getPlayers', players);
+    socket.broadcast.emit('getUsers', users);
   });
-  players.push(new player(socket.id));
-  socket.emit('getPlayers', players);
-  socket.broadcast.emit('getPlayers', players);
+
+  socket.on('setUserReady', function(id) {
+    console.log("Set User Ready" + id);
+    //socket.emit('userReady', { id:socket.id } );
+    //socket.broadcast.emit('userReady', { id:socket.id } );
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].id == id) {
+        if (users[i].isReady == false) {
+          users[i].isReady = true;
+        } else {
+          users[i].isReady = false;
+        }
+      }
+    }
+    socket.emit('getUsers', users);
+    socket.broadcast.emit('getUsers', users);
+  });
+  
+  users.push(new user(socket.id));
+  socket.emit('getUsers', users);
+  socket.broadcast.emit('getUsers', users);
 });
 
-function player(id) {
+function user(id) {
   this.id = id;
+  this.isReady = false;
 }

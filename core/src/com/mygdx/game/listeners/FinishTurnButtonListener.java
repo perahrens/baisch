af7ad7087e65365e1abfe025ff084fdc164c0f2b
perahrens <ahrensper@gmail.com>
2026-03-29 15:48:38 +0200

    fix: player turn rotation

--- core/src/com/mygdx/game/listeners/FinishTurnButtonListener.java
+++ core/src/com/mygdx/game/listeners/FinishTurnButtonListener.java
@@ -1,22 +1,36 @@
 package com.mygdx.game.listeners;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.GameState;
 import com.mygdx.game.Player;
+import io.socket.client.Socket;
 
 public class FinishTurnButtonListener extends ClickListener {
 
   GameState gameState;
+  Socket socket;
 
-  public FinishTurnButtonListener(GameState gameState) {
+  public FinishTurnButtonListener(GameState gameState, Socket socket) {
     this.gameState = gameState;
+    this.socket = socket;
   }
 
   @Override
   public void clicked(InputEvent event, float x, float y) {
-    Player currentPlayer = gameState.getNextPlayer();
-    System.out.println("Next player " + currentPlayer.getPlayerName());
+    Player nextPlayer = gameState.getNextPlayer();
+    System.out.println("Next player " + nextPlayer.getPlayerName());
+    int nextPlayerIndex = gameState.getCurrentPlayerIndex();
+    try {
+      JSONObject data = new JSONObject();
+      data.put("nextPlayerIndex", nextPlayerIndex);
+      socket.emit("finishTurn", data);
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
     gameState.setUpdateState(true);
   };
 }
